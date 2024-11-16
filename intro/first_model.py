67b5bd5557a0b1fc46d77800638ab09dc111eef5
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-11-16 17:01:24 +0100

    Add sols for first exercise set

--- intro/first_model.py
+++ intro/first_model.py
@@ -10,10 +10,14 @@ def first_model():
     #  x, y binary
 
     # TODO: 1. Create variables x and y
+    x = model.addVar(vtype='B', obj=1, name='x')
+    y = model.addVar(vtype='B', obj=1, name='y')
 
     # TODO: 2. Add the constraint x + y <= 1
+    model.addCons(x + y <= 1, name='c1')
 
     # TODO: 3. Set the objective function to maximize x + y
+    model.setMaximize()
 
     return model
 
--- intro/test_print_version.py
+++ intro/test_print_version.py
@@ -4,5 +4,6 @@ from unittest.mock import patch
 @patch('sys.stdout', new_callable=StringIO)
 def test_print_version(mock_stdout):
     from print_version import print_version
+
     print_version()
-    assert "SCIP version" in mock_stdout.getvalue(), "SCIP version is not printed"
+    assert "SCIP version" in mock_stdout.getvalue(), "SCIP version is not printed to stdout"
